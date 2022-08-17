cf495323a65f5186174cabca8376306bdb7da515
AquaApple <yassershehab9@gmail.com>
2022-08-17 17:42:19 +0200

    Created Filter Component and lifted the state up

--- src/components/Expenses/ExpensesList/Expenses.jsx
+++ src/components/Expenses/ExpensesList/Expenses.jsx
@@ -1,21 +1,31 @@
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 import Card from "../../UI/Card/Card";
 import "./Expenses.css";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import { useState } from "react";
 
 function Expenses({ list }) {
+  const [seletedYear, setSelectedYear] = useState("");
+  const selectedYearHanlder = (enteredYear) => {
+    console.log(seletedYear);
+    setSelectedYear(enteredYear);
+  };
   return (
-    <Card className="expenses">
-      {list.map((expense, index) => {
-        return (
-          <ExpenseItem
-            key={index}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
-    </Card>
+    <>
+      <Card className="expenses">
+        <ExpensesFilter onSeletedYear={selectedYearHanlder} seletedYear={seletedYear} />
+        {list.map((expense, index) => {
+          return (
+            <ExpenseItem
+              key={index}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
+      </Card>
+    </>
   );
 }
 
