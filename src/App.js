e0c7899240eb2db79733d9dcbd6350a777bddb3c
AquaApple <yassershehab9@gmail.com>
2022-08-17 17:21:09 +0200

    Added New Form component and Lifted the state up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import ExpenseItem from "./components/Expenses/ExpenseItem/ExpenseItem";
 import Expenses from "./components/Expenses/ExpensesList/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -25,9 +26,14 @@ function App() {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses list={expenses} />
     </div>
   );
---
+++ src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,19 @@
+import "./NewExpense.css";
+import ExpenseForm from "../ExpenseForm/ExpenseForm";
+
+function NewExpense({ onAddExpense }) {
+  const onSaveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    onAddExpense(expenseData);
+  };
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+    </div>
+  );
+}
+
+export default NewExpense;
